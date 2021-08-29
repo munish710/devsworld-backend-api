55a088a8e892bc637f701c3689fcfb5506015f77
munish710 <munish710@gmail.com>
2021-08-29 14:56:43 +0530

    feat : models - add user model

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide Email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a Password"],
+    minLength: 6,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
