2d9b950c7600ad9c8f5b4ef85cf9cffdc512b39e
munish710 <munish710@gmail.com>
2021-08-31 00:42:13 +0530

    refactor : models - Post model, add timestamps

--- models/post.model.js
+++ models/post.model.js
@@ -1,28 +1,33 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-  },
-  body: {
-    type: String,
-    requied: [true, "Body of the post is required"],
-  },
-  imageUrl: {
-    type: String,
-  },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  comments: [
-    {
-      text: String,
-      postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
     },
-  ],
-  postedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    body: {
+      type: String,
+      requied: [true, "Body of the post is required"],
+    },
+    imageUrl: {
+      type: String,
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [
+      {
+        text: String,
+        postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      },
+    ],
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Post", PostSchema);
