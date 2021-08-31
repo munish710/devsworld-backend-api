f68b4a74fc6c36b032222141884181d32128877f
munish710 <munish710@gmail.com>
2021-09-01 00:32:40 +0530

    feat : add various security middlewares

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 require("express-async-errors");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 const app = express();
 
@@ -14,6 +18,11 @@ const postsRouter = require("./routes/posts.router");
 const usersRouter = require("./routes/users.router");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
 
 app.get("/", (req, res) => {
   res.send("Welcome to Social-Media-Backend-API");
